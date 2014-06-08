7e13831fa9a94af18006c938852c08088762936a
Thorsten Lorenz <thlorenz@gmx.de>
2014-06-08 13:23:11 -0400

    logging v8 versions to be included in readme and script to update readme section in index.html

--- build/build.sh
+++ build/build.sh
@@ -30,11 +30,11 @@ do
   v8_v="${versions[$i]}"
   node_v="${versions[$i + 1]}"
 
-  git reset master --hard
-  git clean -f -d
-
   # only update versions we haven't documented yet
   if [ ! -d "$DIR/v8-$v8_v" ]; then
+    git reset master --hard
+    git clean -f -d
+
     git checkout $v8_v                                                                                                    \
       && cat "$DIR/template.doxygen" | sed "s.__root__.$DIR.g; s/__version__/$v8_v/g; s/__node_version__/$node_v/g;" > "$DIR/v8.doxygen" \
       && doxygen "$DIR/v8.doxygen"
@@ -46,3 +46,15 @@ git clean -f -d
 git checkout master
 
 rm $DIR/v8.doxygen
+
+echo 'links to code and docs to be included in readme'
+echo ''
+
+for (( i=0; i<${#versions[@]}; i=$i+2 ));
+do
+  v8_v="${versions[$i]}"
+  node_v="${versions[$i + 1]}"
+
+  echo '- [v8 __version__](https://thlorenz.github.io/v8-perf/build/v8-__version__/html/) | [code](https://github.com/v8/v8/tree/__version__) | [node __node_version__](https://github.com/joyent/node/tree/v__node_version__)' \
+    | sed "s/__version__/$v8_v/g; s/__node_version__/$node_v/g;"
+done
